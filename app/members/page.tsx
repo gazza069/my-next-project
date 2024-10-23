2e1c77c9ec85d05d0ecf28b00785f51dc3cbaee7
satoshi.takasaki <satoshi.takasaki@medicalnote.co.jp>
2024-10-23 12:53:53 +0900

    212ページまで完了

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { getMembersList } from "@/app/_libs/microcms";
-import { MEMBERS_LIST_LIMIT } from "../_contents/index";
+import { MEMBERS_LIST_LIMIT } from "../_constants/index";
 import styles from "./page.module.css";
 
 export default async function Page() {
