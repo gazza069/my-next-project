dc0e298a722f3d4f3098efb6ef2883d746afaa1b
satoshi.takasaki <satoshi.takasaki@medicalnote.co.jp>
2024-10-23 14:30:15 +0900

    importのスペルミス修正

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
 import { notFound } from "next/navigation";
 import NewsList from "@/app/_components/NewsList";
-import Pagenation from "@/app/_components/Pagenation"
+import Pagenation from "@/app/_components/Pagination"
 import Category from "@/app/_components/Category";
 import { NEWS_LIST_LIMIT } from "@/app/_constants"
 
